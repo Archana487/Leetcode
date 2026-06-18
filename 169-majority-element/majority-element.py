7005f25a35dfacab4e12207161df2792fdd4fc6e
Archana V <archanavp2024@gmail.com>
2026-06-18 16:08:43 +0530

    Time: 4 ms (70.63%) | Memory: 21.4 MB (20.58%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count=0
+        for num in nums:
+            if count==0:
+                canditate=num
+            if canditate==num:
+                count+=1    
+            else:
+                count-=1
+        return canditate
